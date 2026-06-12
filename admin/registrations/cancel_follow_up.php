43682aabc95ad35b68674291500a323fc995451c
RicoOrlandoLimardi <ricoorlandolimardi@itbss.ac.id>
2026-06-12 15:27:03 +0700

    Update cancel_follow_up.php

--- admin/registrations/cancel_follow_up.php
+++ admin/registrations/cancel_follow_up.php
@@ -2,7 +2,7 @@
 include "../security.php";
 include "../../koneksi.php";
 
-$registrations_id = $_GET['id'];
+$registration_id = $_GET['id'];
 
 $sql = "update registrations set is_followed_up = 0, followed_up_by=null, followed_up_at=null where id='$registration_id'";
 $query = mysqli_query($conn, $sql) or die($sql);
